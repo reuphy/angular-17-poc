dfa982bd41a522adb8a57ebc3c335a10ea817407
raphael spitz <Rspitz@sterigenics.com>
2023-11-11 18:08:25 +0100

    first project example finish recap : start with new angular 17 project use npx to not break if you have version < 17 also installed new component generated interface import forms module signal for loop new syntaxe @emtpy sintax

--- src/app/appointments-list/appointments-list.component.ts
+++ src/app/appointments-list/appointments-list.component.ts
@@ -1,18 +1,27 @@
 import { Appointment } from './../models/appointment';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-appointments-list',
   standalone: true,
+  //import forms module
   imports: [CommonModule, FormsModule],
   templateUrl: './appointments-list.component.html',
   styleUrl: './appointments-list.component.css'
 })
 export class AppointmentsListComponent {
-  newAppointment:Appointment = {id: 0, title: '', date: new Date()};
-  appointment:Appointment[] = [
-    {id: 1, title: 'Appointment 1', date: new Date('2020-01-01')},
-  ]
+  newAppointment:Appointment = {id: 0, title: '', date: null};
+  // signal array
+  appointments = signal<Appointment[] >([])
+
+  addAppointment() {
+    if (this.newAppointment.title.trim().length === 0 || this.newAppointment.date === null) return;
+    
+    this.appointments().push({...this.newAppointment, id: Date.now()});
+  } 
+  deleteAppointment(appointment: Appointment) {
+    this.appointments().splice(this.appointments().indexOf(appointment), 1);
+  }
 }
