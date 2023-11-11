b6ff55799863de579d17d125dbf73963635e6a57
raphael spitz <Rspitz@sterigenics.com>
2023-11-11 17:18:55 +0100

    adding forms module Testing ng model

--- src/app/appointments-list/appointments-list.component.ts
+++ src/app/appointments-list/appointments-list.component.ts
@@ -1,13 +1,18 @@
+import { Appointment } from './../models/appointment';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-appointments-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './appointments-list.component.html',
   styleUrl: './appointments-list.component.css'
 })
 export class AppointmentsListComponent {
-  test = 'appointments-list works!'
+  newAppointment:Appointment = {id: 0, title: '', date: new Date()};
+  appointment:Appointment[] = [
+    {id: 1, title: 'Appointment 1', date: new Date('2020-01-01')},
+  ]
 }
