51b90e633300f01dee635e9507e78a215e55b73d
raphael spitz <Rspitz@sterigenics.com>
2023-11-13 13:11:18 +0100

    adding angular material theme indigo-pink.css

--- src/app/product-app/product-list/product-list.component.ts
+++ src/app/product-app/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
