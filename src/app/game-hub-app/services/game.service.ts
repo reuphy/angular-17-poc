85410e33dd3a8dbee81db7d833fd7791ed478f4b
raphael spitz <Rspitz@sterigenics.com>
2023-11-28 17:07:19 +0100

    load more with behavior subject. No need to cache data in an extra variable array

--- src/app/game-hub-app/services/game.service.ts
+++ src/app/game-hub-app/services/game.service.ts
@@ -3,8 +3,7 @@ import { FetchGamesData, Games } from '../interfaces/games';
 import { HttpService } from './http.service';
 import { Injectable, inject } from '@angular/core';
 import { FakeGameService } from './mocks/fake-game.service';
-import { Subject, map, pairwise, scan, switchMap, tap } from 'rxjs';
-import { HttpRequestState } from '../interfaces/httpRequestState';
+import { BehaviorSubject, Subject, map, pairwise, scan, switchMap, tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,14 +11,12 @@ export class GameService {
   useMocks = localStorage.getItem('mocks') === 'true';
   httpService = inject(HttpService)
   fakeGameService = inject(FakeGameService)
-  isLoadMore = false;
   private endPoint = '/games'
   private query = ''
-  private refreshGames$ = new Subject<void>();
+  private refreshGames$ = new BehaviorSubject<any>([]);
   params: { [key: string]: string | number } = {};
   page = 1;
   games = toSignal(this.getAll())
-  previousGamesValues: Games[] = [];
 
   getGames() {
     return this.httpService.getAll<FetchGamesData>(this.endPoint, this.query)
@@ -32,13 +29,10 @@ export class GameService {
 
     return this.refreshGames$.pipe(switchMap(() => this.getGames()
       .pipe(
-        map((games
-        ) => {
-          if (this.isLoadMore && games.value?.results) {
-              games.value.results = [...this.previousGamesValues, ...games.value.results]
-              this.isLoadMore = false;
-          }
-          return games
+        map(games => {
+          if (games.value?.results) games.value.results = [...this.refreshGames$.value, ...games.value.results]
+
+          return games;
         })
       )))
   }
@@ -77,19 +71,15 @@ export class GameService {
   }
 
   refreshGames() {
-    if(!this.isLoadMore) {
-      this.setQuery('page', 1);
-      this.previousGamesValues = [];
-    }
-    this.refreshGames$.next();
+    this.setQuery('page', 1);
+
+    this.refreshGames$.next([]);
   }
 
   loadMoreGames() {
     if (this.useMocks) { this.fakeGameService.loadMoreGames(); this.refreshGames() }
 
     this.setQuery('page', ++this.page);
-    this.isLoadMore = true;
-    this.previousGamesValues.push(...(this.games()?.value?.results ?? []));
-    this.refreshGames()
+    this.refreshGames$.next(this.games()?.value?.results ?? []);
   }
 }
