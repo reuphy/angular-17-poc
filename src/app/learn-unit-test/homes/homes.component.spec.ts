c34d8d7c68bfd1602a291b9f0716db97cc8d4c72
raphael spitz <Rspitz@sterigenics.com>
2023-12-24 10:54:36 +0100

    adding home info

--- src/app/learn-unit-test/homes/homes.component.spec.ts
+++ src/app/learn-unit-test/homes/homes.component.spec.ts
@@ -20,6 +20,29 @@ describe('HomesComponent', () => {
   it('should show homes', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelectorAll('[data-test="home"]').length).toBe(3); 
-    jasmine.createSpy
+  });
+
+  it('should show home info', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const home = compiled.querySelector('[data-test="home"]');
+
+    expect(home?.querySelector('[data-test="title"]')
+    ?.textContent
+    ?.toLowerCase()
+    .includes('Home'.toLowerCase()))
+    .toBe(true);
+
+    expect(home?.querySelector('[data-test="location"]')
+    ?.textContent
+    ?.toLowerCase()
+    .includes('new york'.toLowerCase()))
+    .toBe(true);
+
+    expect(home?.querySelector('[data-test="image"]')).toBeTruthy()
+    // ?.innerHTML
+    // ?.toLowerCase()
+    // .includes('assets/images'.toLowerCase())) 
+    // .toBe(true); 
+
   });
 });
--- src/app/learn-unit-test/homes/homes.component.ts
+++ src/app/learn-unit-test/homes/homes.component.ts
@@ -11,7 +11,7 @@ import { of } from 'rxjs';
 })
 export class HomesComponent {
   homes$ = of([
-    { id: 1, title: 'Home 1', image: 'assets/images/home-1.jpg', location: 'Location 1' },
+    { id: 1, title: 'Home 1', image: 'assets/images/home-1.jpg', location: 'new york' },
     { id: 2, title: 'Home 2', image: 'assets/images/home-2.jpg', location: 'Location 2' },
     { id: 3, title: 'Home 3', image: 'assets/images/home-3.jpg', location: 'Location 3' }
   ])
